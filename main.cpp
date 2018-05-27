f53e740835b454820be796d41adeb98babd86b20
qld2 <qld2@illinois.edu>
2018-05-27 14:25:55 -0500

    Learning to test a function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "catch.hpp"
+#define CATCH_CONFIG_MAIN
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int findFibonacci(int index) {
+    if (index == 0 || index == 1) {
+        return 1;
+    }
+
+    return findFibonacci(index - 1) + findFibonacci(index - 2);
+}
+
+TEST_CASE( "Find Fibonacci Number", "[fibonacci]" ) {
+    REQUIRE( findFibonacci(1) == 1 );
+    REQUIRE( findFibonacci(2) == 2 );
+    REQUIRE( findFibonacci(3) == 3 );
+    REQUIRE( findFibonacci(4) == 5 );
+    REQUIRE( findFibonacci(5) == 8 );
+    
 }
