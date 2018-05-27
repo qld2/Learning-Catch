146503ddca3bc71eb6148d012c0970a1a36fc864
qld2 <qld2@illinois.edu>
2018-05-27 13:54:53 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Learning_Catch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Learning_Catch.dir/main.cpp.o"
+  "Learning_Catch.pdb"
+  "Learning_Catch"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Learning_Catch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
